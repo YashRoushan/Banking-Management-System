39d6d1698de3393bd50d66a3e8f67112d9c83937
yash roushan <yashroushan@gmail.com>
2024-08-24 20:15:22 -0300

    uploading new project called Banking Management System

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,15 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login(){
+        setSize(800,480);
+        setVisible(true);
+        setLocation(350, 200);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
